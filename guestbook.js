4ae5f52061e9d0e56d3da65f92e01a7dc0eaa94a
Ddanikhub <100062364+Ddanikhub@users.noreply.github.com>
2022-05-21 02:03:48 -0700

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -7,7 +7,7 @@ const guestbook = {
   get() {
     return $.ajax({
       type: 'GET',
-      url: `${apiUrl}/entries`,
+      url: `https://72133c9b.us-south.apigw.appdomain.cloud/guestbook`,
       dataType: 'json'
     });
   },
